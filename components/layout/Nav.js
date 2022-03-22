1be2be5f3ff7721754c4a9fbe9f6d4fa07c7f4a4
maraj09 <md.marajrashid@gmail.com>
2022-03-22 22:17:26 +0600

    initial commit

---
+++ components/layout/Nav.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import UpWork from "../assets/svg/UpWork";
+import { UilGithub } from "@iconscout/react-unicons";
+import { UilLinkedin } from "@iconscout/react-unicons";
+import { UilSkype } from "@iconscout/react-unicons";
+import { Sling as Hamburger } from "hamburger-react";
+
+const Nav = () => {
+  const [menubarClicked, setMenubarClicked] = useState(false);
+  const [status, setStatus] = useState(0);
+  const handleMenubar = () => {
+    setMenubarClicked(!menubarClicked);
+    setStatus(1);
+  };
+  return (
+    <>
+      <nav>
+        <div
+          className={` nav-links ${
+            menubarClicked
+              ? "nav-links-slide-in"
+              : status == 1
+              ? "nav-links-slide-out"
+              : " "
+          } `}
+        >
+          <a href="">
+            <UpWork className="nav-link-svg" />
+          </a>
+          <a href="">
+            <UilLinkedin className="nav-link-svg" />
+          </a>
+          <a href="">
+            <UilGithub className="nav-link-svg" />
+          </a>
+          <a href="">
+            <UilSkype className="nav-link-svg" />
+          </a>
+        </div>
+
+        <div className="nav-menubar">
+          <Hamburger
+            size={29.9}
+            color={menubarClicked ? "#fff" : "#fbc211"}
+            toggle={() => handleMenubar()}
+            toggled={menubarClicked}
+            duration={0.5}
+          />
+        </div>
+
+        <div className="scrolling-links">
+          <a href="">
+            
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
